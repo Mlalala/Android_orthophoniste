1a85e8dff42d627aba1683cad15ae3ade7eff8c2
lamontagnette <martinez.simon@outlook.fr>
2018-10-18 18:43:39 +0200

    Phonology finished

--- app/src/main/java/fr/insa_cvl/orthophonie/DatabaseAccess.kt
+++ app/src/main/java/fr/insa_cvl/orthophonie/DatabaseAccess.kt
@@ -7,20 +7,11 @@ import android.database.sqlite.SQLiteOpenHelper
 import java.util.ArrayList
 
 class DatabaseAccess
-/**
- * Private constructor to aboid object creation from outside classes.
- *
- * @param context
- */
+
 private constructor(context: Context) {
     private val openHelper: SQLiteOpenHelper
     private var database: SQLiteDatabase? = null
 
-    /**
-     * Read all quotes from the database.
-     *
-     * @return a List of quotes
-     */
     fun get_serie(num_serie : Int, num : Int): ArrayList<String>{
         val list = ArrayList<String>()
         val cursor = database!!.rawQuery("SELECT proposals,answer FROM series WHERE No_serie = " + num_serie.toString()+ " AND No = " + num.toString(), null)
@@ -34,21 +25,23 @@ private constructor(context: Context) {
         return list
     }
 
+    fun count(num_serie : Int):Int{
+        val cursor = database!!.rawQuery("SELECT COUNT(No)\n" +
+                "FROM series\n" +
+                "WHERE No_serie = " + num_serie.toString(),null)
+        cursor.moveToFirst()
+        return cursor.getInt(0)
+    }
 
     init {
         this.openHelper = DatabaseOpenHelper(context)
     }
 
-    /**
-     * Open the database connection.
-     */
+
     fun open() {
         this.database = openHelper.writableDatabase
     }
 
-    /**
-     * Close the database connection.
-     */
     fun close() {
         if (database != null) {
             this.database!!.close()
@@ -58,12 +51,6 @@ private constructor(context: Context) {
     companion object {
         private var instance: DatabaseAccess? = null
 
-        /**
-         * Return a singleton instance of DatabaseAccess.
-         *
-         * @param context the Context
-         * @return the instance of DabaseAccess
-         */
         fun getInstance(context: Context): DatabaseAccess {
             if (instance == null) {
                 instance = DatabaseAccess(context)
--- app/src/main/java/fr/insa_cvl/orthophonie/PhonologyActivity.kt
+++ app/src/main/java/fr/insa_cvl/orthophonie/PhonologyActivity.kt
@@ -6,12 +6,22 @@ import android.support.v7.app.AppCompatActivity
 import android.view.KeyEvent
 import android.widget.*
 import android.media.MediaPlayer
+import android.app.AlertDialog
+import android.view.Gravity
+import android.widget.TextView
+import android.view.WindowManager
+import android.util.DisplayMetrics
+
 
 
 class PhonologyActivity : AppCompatActivity(){
 
     private var index_in_serie : Int = 0
     private var index_serie : Int = 0
+    private var proposal : List<String>? = null
+    private var answer : Int = 0
+
+    private var length_serie : Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,23 +29,29 @@ class PhonologyActivity : AppCompatActivity(){
         setContentView(R.layout.phonology_layout)
         index_serie = intent.getIntExtra("EXTRA_POSITION",0)
 
-        val proposal : List<String>
-        val answer : Int
-
+        //Serie lengh
         val databaseAccess = DatabaseAccess.getInstance(this)
         databaseAccess.open()
-        val sql_query = databaseAccess.get_serie(index_serie+1,index_in_serie+1)
+        var sql_query = databaseAccess.count(index_serie+1)
+        length_serie = sql_query.toInt()
+        databaseAccess.close()
+
+        //init proposals, answer and audio
+        init_serie_data()
+        //Play audio one time
+        MediaPlayer.create(this, getResources().getIdentifier("audio"+(index_serie+1).toString() +(index_in_serie+1).toString(),"raw","fr.insa_cvl.orthophonie")).start()
+        //Change all the strings and audio of the current serie item
+        display_phono(proposal!!, answer)
+    }
+
+    fun init_serie_data(){
+        val databaseAccess = DatabaseAccess.getInstance(this)
+        databaseAccess.open()
+        var sql_query = databaseAccess.get_serie(index_serie+1,index_in_serie+1)
         databaseAccess.close()
 
         proposal = sql_query[0].toString().split("-")
         answer = sql_query[1].toInt()
-
-        //Play audio one time
-        MediaPlayer.create(this, getResources().getIdentifier("audio"+(index_serie+1).toString() +(index_in_serie+1).toString(),"raw","fr.insa_cvl.orthophonie")).start()
-        //Change all the strings and audio of the current serie item
-        display_phono(proposal, answer)
-
-
     }
 
     fun display_phono(proposals : List<String>, answer: Int) {
@@ -68,11 +84,87 @@ class PhonologyActivity : AppCompatActivity(){
 
     fun isCorrect(answer : Int, response : Int){
         if (answer == response) {
-            Toast.makeText(this, "Correct : A = " + answer.toString() + "| R = " + response.toString() , Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Correct" + answer.toString() + "| R = " + response.toString() , Toast.LENGTH_SHORT).show()
+            manageItem("Correct")
         }
         else{
-            Toast.makeText(this, "False : A = " + answer.toString() + "| R = " + response.toString(), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "False" + answer.toString() + "| R = " + response.toString(), Toast.LENGTH_SHORT).show()
+            manageItem("Faux")
         }
+    }
+
+    fun manageItem(title : String) {
+        val builder = AlertDialog.Builder(this)
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.alert_phonology_layout, null)
+
+        //builder.setTitle(title).setView(dialogView)
+
+        val myMsg = TextView(this)
+        myMsg.text = title
+        myMsg.gravity = Gravity.CENTER
+        myMsg.textSize = 35f
+        myMsg.setTextColor(resources.getColor(R.color.alertcolour))
+
+        //set custom title
+        builder.setCustomTitle(myMsg)
+
+        // Add the buttons
+        if (title == "Correct"){
+            if (index_in_serie < length_serie - 1){
+                builder.setPositiveButton("Suivant") { dialog, id ->
+                    index_in_serie += 1
+                    //init proposals, answer and audio
+                    init_serie_data()
+                    //Play audio one time
+                    MediaPlayer.create(this, getResources().getIdentifier("audio"+(index_serie+1).toString() +(index_in_serie+1).toString(),"raw","fr.insa_cvl.orthophonie")).start()
+                    //Change all the strings and audio of the current serie item
+                    display_phono(proposal!!, answer)
+                }
+            }
+            else {
+                builder.setPositiveButton("Revenir au menu") { dialog, id ->
+                    val intent = Intent(this, PhonologyMenuActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+
+        // Create the AlertDialog
+        val dialog = builder.create()
+        dialog.show()
+
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        // The absolute width of the available display size in pixels.
+        val displayWidth = displayMetrics.widthPixels
+
+        // Initialize a new window manager layout parameters
+        val layoutParams = WindowManager.LayoutParams()
+
+        // Copy the alert dialog window attributes to new layout parameter instance
+        layoutParams.copyFrom(dialog.window.attributes)
+
+        // Set the alert dialog window width and height
+        // Set alert dialog width equal to screen width 90%
+        // int dialogWindowWidth = (int) (displayWidth * 0.9f);
+        // Set alert dialog height equal to screen height 90%
+        // int dialogWindowHeight = (int) (displayHeight * 0.9f);
+
+        // Set alert dialog width equal to screen width 70%
+        val dialogWindowWidth = (displayWidth * 0.65f).toInt()
+
+
+        // Set the width and height for the layout parameters
+        // This will bet the width and height of alert dialog
+        layoutParams.width = dialogWindowWidth
+        //layoutParams.height = dialogWindowHeight
+
+        // Apply the newly created layout parameters to the alert dialog window
+        dialog.window.attributes = layoutParams
+
     }
 
 
