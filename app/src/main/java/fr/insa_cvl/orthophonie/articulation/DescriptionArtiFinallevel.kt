f2da12005e3bf67fcfc667bc17ba7ec4a1a80a5c
Mysthle <maximelarrieu91@gmail.com>
2018-11-08 16:34:04 +0100

    ajout retour activity

--- app/src/main/java/fr/insa_cvl/orthophonie/articulation/DescriptionArtiFinallevel.kt
+++ app/src/main/java/fr/insa_cvl/orthophonie/articulation/DescriptionArtiFinallevel.kt
@@ -1,7 +1,10 @@
 package fr.insa_cvl.orthophonie.articulation
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import fr.insa_cvl.orthophonie.MainActivity
 import fr.insa_cvl.orthophonie.R
 
 class DescriptionArtiFinallevel: AppCompatActivity() {
@@ -9,5 +12,15 @@ class DescriptionArtiFinallevel: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.articulation_layout)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        return if (keyCode == KeyEvent.KEYCODE_BACK) {
+            val intent = Intent(this, DescriptionArtiLevel::class.java)
+            startActivity(intent)
+            finish()
+            true
+        } else super.onKeyDown(keyCode, event)
+
     }
 }
